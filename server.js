125281c0d93805cbd3f27d28b12bc7f484c15ed5
bleams <mbella.georges@gmail.com>
2016-02-06 19:16:40 +0100

    introducing the ui

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(express.static(__dirname + '/public'));
 
 if(env === 'development') {
     mongoose.connect('mongodb://localhost/superapp');
+    app.locals.pretty = true;
 } else {
     mongoose.connect('mongodb://geobleam:$obleam0@ds055905.mongolab.com:55905/superapp');
 }
@@ -29,13 +30,6 @@ db.once('open', function () {
     console.log('superapp db opened');
 });
 
-var messageSchema = mongoose.Schema({message: String});
-var Message = mongoose.model('Message', messageSchema);
-var mongoMessage;
-Message.findOne().exec(function (err, messageDoc) {
-    mongoMessage = messageDoc.message;
-});
-
 
 app.get('/partials/:partialPath', function (req, res) {
     res.render('partials/' + req.params.partialPath);
@@ -43,9 +37,7 @@ app.get('/partials/:partialPath', function (req, res) {
 });
 
 app.get('*', function (req, res) {
-    res.render('index', {
-        mongoMessage: mongoMessage
-    });
+    res.render('index');
 });
 
 var port = process.env.PORT || 3030;
